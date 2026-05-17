c386cf7379b997e3c646a0c7168fa389ba9e7b11
Moacir <samoel.anhaia@outlook.com>
2026-05-17 20:39:45 -0300

    atualizado

--- 3-Semestre/2-Bimestre/desafio-final-rpg/src/main/kotlin/com/unipar/desafio_final_rpg/model/Personagem.kt
+++ 3-Semestre/2-Bimestre/desafio-final-rpg/src/main/kotlin/com/unipar/desafio_final_rpg/model/Personagem.kt
@@ -1,89 +1,54 @@
 package com.unipar.desafio_final_rpg.model
 
+import jakarta.persistence.DiscriminatorColumn
+import jakarta.persistence.DiscriminatorValue
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Inheritance
+import jakarta.persistence.InheritanceType
+import kotlin.random.Random
 
 @Entity
-data class Personagem(
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // Define estratégia de herança em uma unica tabela
+@DiscriminatorColumn(name = "tipo_personagem") // Nome da coluna no BD
+open class Personagem(
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.SEQUENCE)
-    val nome: String,
-    var forca: Double,
-    var velocidade: Double,
-    var vida: Double,
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    var id: Long? = null, // id gerenciado automatico atraves de sequencia 0.1.2.3
+    var nome: String = "",
+    var forca: Double = 0.0,
+    var velocidade: Double = 0.0,
+    var vida: Double = 0.0
 )
 
+@Entity
+@DiscriminatorValue("GUERREIRO")
+class Guerreiro(
+    nome: String = "Guerreiro",
+    forca: Double = 100.0,
+    velocidade: Double = 100.0,
+    vida: Double = 100.0,
+    var defesa: Double = Random.nextDouble(50.0, 200.0) // atributo unico
+) : Personagem(null, nome, forca, velocidade, vida)
 
+@Entity
+@DiscriminatorValue("MAGO")
+class Mago(
+    nome: String = "Mago",
+    forca: Double = 100.0,
+    velocidade: Double = 100.0,
+    vida: Double = 100.0,
+    var magia: Double = Random.nextDouble(50.0, 200.0) // atributo unico
+) : Personagem(null, nome, forca, velocidade, vida)
 
-
-
-
-/*
-   open fun usarPoder(monstro: Monstro) {
-        println("O herói usou o poder especial!!")
-    }
-
-    open fun atacar(monstro: Monstro) {
-        println("$nome ataca ${monstro.nome}")
-        if (this.velocidade >= monstro.velocidade) {
-            println("Você é mais rapido, acertando o ataque.")
-            monstro.vida -= this.forca
-            println("Vida do ${monstro.nome} foi para: ${monstro.vida}")
-        } else {
-            println("O ${monstro.nome} desviou do seu ataque!")
-        }
-    }
-
-    open fun defender(monstro: Monstro) {
-        println(" $nome tentando se defender ${monstro.nome} ")
-        if (this.velocidade > monstro.forca) {
-            println("Você se defendeu do ataque")
-        } else {
-            println("Você falhou em se defender")
-            this.vida -= monstro.forca
-            println("Sua vida caiu para ${this.vida}")
-        }
-    }
-}
-
-class Guerreiro(var defesa: Double = 150.0) : Personagem(
-    nome = "Guerreiro",
-    forca = 100.0,
-    velocidade = 100.0,
-    vida = 100.0
-) {
-    override fun usarPoder(monstro: Monstro) {
-        if (this.defesa >= monstro.forca) {
-            this.defesa -= monstro.forca
-            println("Usou a defesa!!")
-        }
-    }
-}
-
-class Mago(var magia: Double = Random.nextDouble(20.0,180.0)) : Personagem(
-    nome = "Mago",
-    forca = 100.0,
-    velocidade = 100.0,
-    vida = 100.0
-) {
-    override fun usarPoder(monstro: Monstro) {
-        val poderespecial = this.magia + this.forca
-        monstro.vida -= poderespecial
-        println("Usou o poder especial, reduzindo a Vvida do ${monstro.nome} para ${monstro.vida}")
-    }
-}
-
-class Ladino(var sagacidade: Double = Random.nextDouble(50.0,150.0)) : Personagem(
-    nome = "Ladino",
-    forca = 100.0,
-    velocidade = 100.0,
-    vida = 100.0
-) {
-    override fun usarPoder(monstro: Monstro) {
-        println("Usou um ataque furtivo!")
-        monstro.vida -= this.sagacidade + this.forca
-        println("Reduzindo a vida do ${monstro.nome} para ${monstro.vida}")
-    }
-}
-* */
+@Entity
+@DiscriminatorValue("LADINO")
+class Ladino(
+    nome: String = "Ladino",
+    forca: Double = 100.0,
+    velocidade: Double = 100.0,
+    vida: Double = 100.0,
+    var sagacidade: Double = Random.nextDouble(50.0, 200.0) // atributo unico
+) : Personagem(null, nome, forca, velocidade, vida)
